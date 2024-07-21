dd4fe9186884f2e2eb12587dfb9d16e0242201b8
ere erer <sjorsmallon@gmail.com>
2024-07-21 11:38:26 +0200

    IR generation

--- kaleidoscope/parser.cc
+++ kaleidoscope/parser.cc
@@ -1,11 +1,23 @@
-#ifndef INCLUDED_TOKENIZER_
-#define INCLUDED_TOKENIZER_
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+
 #include <string>
 #include <vector>
 #include <memory>
 #include <map>
 
 using namespace std;
+using namespace llvm; 
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -25,13 +37,6 @@ static string g_identifier_string; // Filled in if TOKEN_IDENTIFIER (wow, so not
 static double g_number_value;  
 
 
-
-
-
-
-
-
-
 //===----------------------------------------------------------------------===//
 // Abstract Syntax Tree (aka Parse Tree)
 //===----------------------------------------------------------------------===//
@@ -39,40 +44,53 @@ static double g_number_value;
 namespace {
 
 /// expr_ast - Base class for all expression nodes.
-class expr_ast {
+class expr_ast
+{
 public:
-  virtual ~expr_ast() = default;
+    virtual ~expr_ast() = default;
+    virtual Value* codegen() = 0; // inheritance != polymorphism :~)
 };
 
 /// number_expr_ast - Expression class for numeric literals like "1.0".
-class number_expr_ast : public expr_ast {
-  double Val;
+class number_expr_ast : public expr_ast
+{
+  double value;
 
 public:
-  number_expr_ast(double Val) : Val(Val) {}
+  number_expr_ast(double value) : value(value) {}
+  Value* codegen() override;
+
 };
 
+
+
 /// variable_expr_ast - Expression class for referencing a variable, like "a".
-class variable_expr_ast : public expr_ast {
-  std::string name;
+class variable_expr_ast : public expr_ast
+{
+    std::string name;
 
 public:
-  variable_expr_ast(const std::string &name) : name(name) {}
+    variable_expr_ast(const std::string &name) : name(name) {}
+    Value* codegen() override;
 };
 
 /// binary_expr_ast - Expression class for a binary operator.
-class binary_expr_ast : public expr_ast {
-  char Op;
-  std::unique_ptr<expr_ast> LHS, RHS;
+class binary_expr_ast : public expr_ast
+{
+  char op;
+  std::unique_ptr<expr_ast> lhs, rhs;
 
 public:
-  binary_expr_ast(char Op, std::unique_ptr<expr_ast> LHS,
-                std::unique_ptr<expr_ast> RHS)
-      : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+    binary_expr_ast(char op, std::unique_ptr<expr_ast> lhs,
+                std::unique_ptr<expr_ast> rhs)
+      : op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {}
+
+    Value* codegen() override;
 };
 
 /// call_expr_ast - Expression class for function calls.
-class call_expr_ast : public expr_ast {
+class call_expr_ast : public expr_ast
+{
   std::string callee;
   std::vector<std::unique_ptr<expr_ast>> arguments;
 
@@ -80,12 +98,16 @@ public:
   call_expr_ast(const std::string &callee,
               std::vector<std::unique_ptr<expr_ast>> arguments)
       : callee(callee), arguments(std::move(arguments)) {}
+
+
+    Value* codegen() override;
 };
 
 /// prototype_ast - This class represents the "prototype" for a function,
 /// which captures its name, and its argument names (thus implicitly the number
 /// of arguments the function takes).
-class prototype_ast {
+class prototype_ast
+{
   std::string name;
   std::vector<std::string> arguments;
 
@@ -93,7 +115,9 @@ public:
   prototype_ast(const std::string &name, std::vector<std::string> arguments)
       : name(name), arguments(std::move(arguments)) {}
 
-  const std::string &getName() const { return name; }
+
+    Function* codegen();
+    const std::string &getName() const { return name; }
 };
 
 /// function_ast - This class represents a function definition itself.
@@ -105,6 +129,8 @@ public:
   function_ast(std::unique_ptr<prototype_ast> prototype,
               std::unique_ptr<expr_ast> body)
       : prototype(std::move(prototype)), body(std::move(body)) {}
+
+    Function* codegen();
 };
 
 } // end anonymous namespace
@@ -184,17 +210,20 @@ static int get_next_token() {
 }
 
 /// log_error* - These are little helper functions for error handling.
-std::unique_ptr<expr_ast> log_error(const char *message) {
+std::unique_ptr<expr_ast> log_error(const char *message)
+{
   fprintf(stderr, "Error: %s\n", message);
   return nullptr;
 }
-std::unique_ptr<prototype_ast> log_error_p(const char *message) {
+std::unique_ptr<prototype_ast> log_error_p(const char *message)
+{
   log_error(message);
   return nullptr;
 }
 
+
 // Parser stuff
-\
+
 
 // awful forward declaration because of circular thbngs.
 static std::unique_ptr<expr_ast> parse_expression();
@@ -437,69 +466,240 @@ static std::unique_ptr<prototype_ast> parse_extern()
 }
 
 
-
-
 //===----------------------------------------------------------------------===//
-// Top-Level parsing
+// Code Generation
 //===----------------------------------------------------------------------===//
 
-static void handle_definition() {
-  if (parse_definition()) {
-    fprintf(stderr, "Parsed a function definition.\n");
-  } else {
-    // Skip token for error recovery.
-    get_next_token();
+static std::unique_ptr<LLVMContext> llvm_context;
+static std::unique_ptr<IRBuilder<>> ir_builder;
+static std::unique_ptr<Module> module;
+static std::map<std::string, Value*> named_values;
+
+Value* log_error_v(const char *message)
+{
+  log_error(message);
+  return nullptr;
+}
+
+Value* number_expr_ast::codegen()
+{
+  return ConstantFP::get(*llvm_context, APFloat(this->value)); // now i'm using "this"!
+}
+
+
+Value* variable_expr_ast::codegen()
+{
+  // Look this variable up in the function.
+  Value *value = named_values[name];
+  if (!value) log_error_v("Unknown variable name");
+
+  return value;
+}
+
+
+
+Value* binary_expr_ast::codegen()
+{
+
+  Value* lhs_value = lhs->codegen();
+  Value* rhs_value = rhs->codegen();
+
+  if (!lhs_value || !rhs_value) return nullptr;
+
+  switch (op) {
+  case '+':
+    return ir_builder->CreateFAdd(lhs_value, rhs_value, "addtmp");
+  case '-':
+    return ir_builder->CreateFSub(lhs_value, rhs_value, "subtmp");
+  case '*':
+    return ir_builder->CreateFMul(lhs_value, rhs_value, "multmp");
+  case '<':
+    lhs_value = ir_builder->CreateFCmpULT(lhs_value, rhs_value, "cmptmp");
+    // Convert bool 0/1 to double 0.0 or 1.0
+    return ir_builder->CreateUIToFP(lhs_value, Type::getDoubleTy(*llvm_context),
+                                 "booltmp");
+  default:
+    return log_error_v("invalid binary operator");
   }
 }
 
-static void handle_extern() {
-  if (parse_extern()) {
-    fprintf(stderr, "Parsed an extern\n");
-  } else {
-    // Skip token for error recovery.
-    get_next_token();
+Value* call_expr_ast::codegen() {
+  // Look up the name in the global module table.
+  Function* callee_function = module->getFunction(this->callee);
+
+  if (!callee_function) return log_error_v("Unknown function referenced");
+
+  // If argument mismatch error.
+  if (callee_function->arg_size() != this->arguments.size()) return log_error_v("Incorrect # arguments passed");
+
+  // wow, what is even going on here
+  std::vector<Value*> value_arguments;
+  for (unsigned i = 0, e = value_arguments.size(); i != e; ++i)
+  {
+    value_arguments.push_back(this->arguments[i]->codegen());
+    if (!value_arguments.back()) return nullptr;
   }
+
+  return ir_builder->CreateCall(callee_function, value_arguments, "calltmp");
+}
+
+
+
+Function* prototype_ast::codegen()
+{
+    // Make the function type:  double(double,double) etc.
+    std::vector<Type*> doubles(
+        this->arguments.size(),
+        Type::getDoubleTy(*llvm_context));
+
+
+    FunctionType* function_type = FunctionType::get(Type::getDoubleTy(*llvm_context), doubles, false);
+
+    Function* function = Function::Create(function_type, Function::ExternalLinkage, this->name, module.get());
+
+    // Set names for all arguments.
+    unsigned idx = 0;
+    for (auto &argument : function->args())
+    {
+      argument.setName(this->arguments[idx++]);
+    }
+    return function;
+}
+
+Function* function_ast::codegen()
+{
+    // First, check for an existing function from a previous 'extern' declaration.
+    Function* function = module->getFunction(this->prototype->getName());
+
+    if (!function) function = this->prototype->codegen();
+
+    if (!function) return nullptr;
+
+    if (!function->empty()) return (Function*)log_error_v("Function cannot be redefined.");
+
+
+    // Create a new basic block to start insertion into.
+    BasicBlock* basic_block = BasicBlock::Create(*llvm_context, "entry", function);
+    ir_builder->SetInsertPoint(basic_block);
+
+    // Record the function arguments in the NamedValues map.
+    named_values.clear();
+    for (auto &arg : function->args())
+    {
+        named_values[std::string(arg.getName())] = &arg;
+    }
+
+
+    if (Value *RetVal = this->body->codegen())
+    {
+        // Finish off the function.
+        ir_builder->CreateRet(RetVal);
+
+        // Validate the generated code, checking for consistency.
+        verifyFunction(*function);
+
+        return function;
+    }
+
+    // Error reading body, remove function.
+    function->eraseFromParent();
+    return nullptr;
+}
+
+
+
+//===----------------------------------------------------------------------===//
+// Top-Level parsing and JIT driver
+//===----------------------------------------------------------------------===//
+
+static void initialize_module()
+{
+  // Open a new context and module.
+  llvm_context = std::make_unique<LLVMContext>();
+  module = std::make_unique<Module>("my cool jit", *llvm_context);
+
+  // Create a new ir_builder for the module.
+  ir_builder = std::make_unique<IRBuilder<>>(*llvm_context);
+}
+
+static void handle_definition()
+{
+    if (auto function_ast = parse_definition())
+    {
+        if (auto *function_ir = function_ast->codegen()) 
+        {
+            fprintf(stderr, "Read function definition:");
+            function_ir->print(errs());
+            fprintf(stderr, "\n");
+        }
+      
+    } else
+    {
+        // Skip token for error recovery.
+        get_next_token();
+    }
+}
+
+static void handle_extern()
+{
+    if (auto prototype_ast = parse_extern())
+    {
+        if (auto* prototype_ir = prototype_ast->codegen())
+        {
+            fprintf(stderr, "read extern\n");
+            prototype_ir->print(errs()); // this just means: print to stderr.
+            fprintf(stderr, "\n"); 
+        }
+    } else
+    {
+        // Skip token for error recovery.
+        get_next_token();
+    }
 }
 
 static void handle_top_level_expression() {
   // Evaluate a top-level expression into an anonymous function.
-  if (parse_top_level_expr()) {
-    fprintf(stderr, "Parsed a top-level expr\n");
-  } else {
+  if (auto top_level_expr_function_ast = parse_top_level_expr())
+  {
+    if (auto* top_level_expr_function_ir = top_level_expr_function_ast->codegen())
+    {
+        fprintf(stderr, "read top-level-expression\n");
+        top_level_expr_function_ir->print(errs());
+        fprintf(stderr, "\n");
+
+        top_level_expr_function_ir->eraseFromParent();
+    }
+  } else
+  {
     // Skip token for error recovery.
     get_next_token();
   }
 }
-
-
-
 
 
 /// top ::= definition | external | expression | ';'
 static void main_loop()
 {
-  while (true) {
-    fprintf(stderr, "ready> ");
-    switch (g_current_token) {
-    case TOKEN_EOF:
-      return;
-    case ';': // ignore top-level semicolons.
-      get_next_token();
-      break;
-    case TOKEN_DEF:
-      handle_definition();
-      break;
-    case TOKEN_EXTERN:
-      handle_extern();
-      break;
-    default:
-      handle_top_level_expression();
-      break;
+    while (true) {
+        fprintf(stderr, "ready> ");
+        switch (g_current_token) {
+        case TOKEN_EOF:
+        return;
+        case ';': // ignore top-level semicolons.
+        get_next_token();
+        break;
+        case TOKEN_DEF:
+        handle_definition();
+        break;
+        case TOKEN_EXTERN:
+        handle_extern();
+        break;
+        default:
+        handle_top_level_expression();
+        break;
     }
   }
 }
-
-
 
 
 
@@ -519,13 +719,13 @@ int main()
 
     get_next_token();
 
+    // Make the module, which holds all the code.
+    initialize_module();
+
     main_loop();
 
+  // Print out all of the generated code.
+    module->print(errs(), nullptr);
 
     return 0;
 }
-
-
-
-
-#endif
